fcee431108f644c3ce9b2ea326f0b0b83c2f0d6d
Alexa-Web-com <aleksandra.wilczynska.plgdy@gmail.com>
2026-02-15 21:36:04 +0100

    change logo, remove gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/podologiagdynia/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
